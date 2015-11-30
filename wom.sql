df33f02c5680edd7c9ecd23ea97faa79bcc02b38
mgerb42 <mgerb42@gmail.com>
2015-11-30 08:35:04 -0600

    updated wom.sql

--- wom.sql
+++ wom.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2015 at 07:15 PM
+-- Generation Time: Nov 30, 2015 at 03:34 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -28,18 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `guild` (
   `guild_title` varchar(35) NOT NULL,
-  `char_name` varchar(45) NOT NULL,
+  `account_number` varchar(45) NOT NULL,
   `leaderflag` tinyint(1) NOT NULL,
-  PRIMARY KEY (`guild_title`,`char_name`),
+  PRIMARY KEY (`account_number`),
   UNIQUE KEY `guild_title` (`guild_title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `guild`
---
-
-INSERT INTO `guild` (`guild_title`, `char_name`, `leaderflag`) VALUES
-('Subway eat Flesh', 'Jegaar', 1);
 
 -- --------------------------------------------------------
 
@@ -49,19 +42,13 @@ INSERT INTO `guild` (`guild_title`, `char_name`, `leaderflag`) VALUES
 
 CREATE TABLE IF NOT EXISTS `purchase_table` (
   `account_number` int(11) NOT NULL,
-  `card_number` varchar(11) NOT NULL,
+  `card_number` varchar(20) NOT NULL,
   `card_type` varchar(35) NOT NULL,
-  `transaction_amt` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `date` varchar(20) NOT NULL,
   PRIMARY KEY (`account_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `purchase_table`
---
-
-INSERT INTO `purchase_table` (`account_number`, `card_number`, `card_type`, `transaction_amt`, `date`) VALUES
-(121, '223-2322-23', 'Visa', 30, '2015-11-02');
 
 -- --------------------------------------------------------
 
@@ -70,19 +57,64 @@ INSERT INTO `purchase_table` (`account_number`, `card_number`, `card_type`, `tra
 --
 
 CREATE TABLE IF NOT EXISTS `quests` (
+  `quest_id` int(11) NOT NULL,
   `quest_title` varchar(125) NOT NULL,
-  `completion` tinyint(1) NOT NULL DEFAULT '0',
-  `quest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `quest_reqlvl` int(11) NOT NULL,
   PRIMARY KEY (`quest_id`),
   UNIQUE KEY `quest_id_UNIQUE` (`quest_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `quests`
 --
 
-INSERT INTO `quests` (`quest_title`, `completion`, `quest_id`) VALUES
-('Lucky Hearts', 1, 1);
+INSERT INTO `quests` (`quest_id`, `quest_title`, `quest_reqlvl`) VALUES
+(1, 'Lucky Hearts', 0),
+(2, 'Sharptalon''s Claw', 19),
+(11, 'Riverpaw Gnoll Bounty', 7),
+(16, 'Give Gerard a Drink', 1),
+(25, 'Simmer Down Now', 23),
+(35, 'Further Concerns', 7),
+(37, 'Find the Lost Guards', 7),
+(40, 'A Fishy Peril', 7),
+(46, 'Bounty on Murlocs', 7),
+(52, 'Protect the Frontier', 7),
+(54, 'Report to Goldshire', 1),
+(59, 'Cloth and Leather Armor', 7),
+(62, 'The Fargodeep Mine', 4),
+(71, 'Report to Thomas', 7),
+(76, 'The Jasperlode Mine', 4),
+(83, 'Fine Linen Goods', 7),
+(84, 'Back to Billy', 5),
+(85, 'Lost Necklace', 5),
+(86, 'Pie for Billy', 5),
+(87, 'Goldtooth', 5),
+(88, 'Princess Must Die!', 6),
+(106, 'Young Lovers', 5),
+(107, 'Note to William', 5),
+(111, 'Speak with Gramma', 5),
+(112, 'Collecting Kelp', 5),
+(114, 'The Escape', 5),
+(123, 'The Collector', 7),
+(147, 'Manhunt', 7),
+(171, 'A Warden of the Alliance', 10),
+(172, 'Children''s Week', 10),
+(176, 'Wanted:  "Hogger"', 7),
+(185, 'Tiger Hunting', 24),
+(186, 'Tiger Stalking', 24),
+(187, 'Tiger Prowess', 24),
+(188, 'Tiger Mastery', 24),
+(190, 'Panther Hunting', 24),
+(191, 'Panther Stalking', 24),
+(192, 'Panther Prowess', 24),
+(193, 'Panther Mastery', 24),
+(194, 'Raptor Hunting', 24),
+(195, 'Raptor Stalking', 24),
+(196, 'Raptor Prowess', 24),
+(197, 'Raptor Mastery', 24),
+(29037, '''Borrowing'' From the Winterfall', 50),
+(38396, 'A Curious Oddity', 100),
+(39665, 'A True Naval Commander', 100);
 
 -- --------------------------------------------------------
 
@@ -95,18 +127,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_name` varchar(45) DEFAULT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
   PRIMARY KEY (`account_number`),
   UNIQUE KEY `account_number_UNIQUE` (`account_number`),
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `user_name_2` (`user_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=126 ;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`account_number`, `user_name`, `first_name`, `last_name`) VALUES
-(125, NULL, 'NAMESET', 'NAMELAST');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -115,21 +141,14 @@ INSERT INTO `user` (`account_number`, `user_name`, `first_name`, `last_name`) VA
 --
 
 CREATE TABLE IF NOT EXISTS `user_data` (
-  `char_name` varchar(45) NOT NULL,
-  `char_level` int(11) NOT NULL,
-  `location_x` int(11) NOT NULL,
-  `location_y` int(11) NOT NULL,
   `account_number` int(11) NOT NULL,
-  PRIMARY KEY (`account_number`,`char_name`),
+  `char_level` int(11) NOT NULL,
+  `quests_completed` int(10) NOT NULL DEFAULT '0',
+  `image_path` varchar(30) NOT NULL,
+  PRIMARY KEY (`account_number`),
+  UNIQUE KEY `account_number` (`account_number`),
   KEY `account_number_idx` (`account_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_data`
---
-
-INSERT INTO `user_data` (`char_name`, `char_level`, `location_x`, `location_y`, `account_number`) VALUES
-('Jegaar', 1, 33, 25, 121);
 
 -- --------------------------------------------------------
 
@@ -138,17 +157,11 @@ INSERT INTO `user_data` (`char_name`, `char_level`, `location_x`, `location_y`, 
 --
 
 CREATE TABLE IF NOT EXISTS `user_friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `account_number` int(11) NOT NULL,
   `friend_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`account_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_friends`
---
-
-INSERT INTO `user_friends` (`account_number`, `friend_name`) VALUES
-(121, 'Jos');
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -157,18 +170,12 @@ INSERT INTO `user_friends` (`account_number`, `friend_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user_quests` (
-  `char_name` varchar(45) NOT NULL,
+  `account_number` int(11) NOT NULL,
   `quest_id` int(11) NOT NULL,
-  PRIMARY KEY (`char_name`,`quest_id`),
+  `completion` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`quest_id`),
   KEY `quest_id_idx` (`quest_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_quests`
---
-
-INSERT INTO `user_quests` (`char_name`, `quest_id`) VALUES
-('Jegaar', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
